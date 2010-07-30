5ea4aa73bac7d7b32e453efd946b602f7f71ebcb
Francois Bleibel <fbleibel@gmail.com>
2010-07-30 11:13:53 +0900

    Changed the lib names listed in the .pc

--- sot-dynamic.pc.cmake
+++ sot-dynamic.pc.cmake
@@ -1,6 +1,6 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 exec_prefix=${install_pkg_prefix}
-libdir=${install_pkg_exec_prefix}/lib
+libdir=${install_pkg_exec_prefix}/lib/plugin
 includedir=${install_pkg_prefix}/include
 datarootdir=${install_pkg_prefix}/share
 docdir=${install_pkg_datarootdir}/doc/${PROJECT_NAME}
