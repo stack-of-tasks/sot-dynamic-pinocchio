1049f493e5b65a580d6236101595d24ac2e1e7e4
Thomas Flayols <thomas.flayols@gmail.com>
2015-04-17 10:30:06 +0200

    add a test file for developpment 1 Arg: URDF Path

---
+++ unitTesting/dev_test.cpp
@@ -0,0 +1,18 @@
+#include <sot-dynamic/dynamic.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace dynamicgraph::sot;
+
+int main(int argc, char * argv[])
+{
+    cout<<"tests sot-dynamic-pinocchio"<<endl;
+    if (argc!=5)
+    {
+        cerr << "Wrong argument number: expect 1 got " << argc-1 <<endl;
+        cerr << "Usage:" << endl;
+        cerr << "./" << argv[0] << " PATH_TO_URDF_FILE" << endl;
+        cerr << "\tPATH_TO_URDF_FILE : Path to the URDF model file describing the robot. "<< endl;
+    }
+}
